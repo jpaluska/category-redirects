747a5b7817f59787499371b248822f43b41603d5
Jacob Paluska-Yakel <91086572+jpaluska@users.noreply.github.com>
2022-01-03 13:11:44 -0700

    Added comments to updated category_redirects.py

--- category-redirects.py
+++ category-redirects.py
@@ -7,154 +7,166 @@ import re
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#Number of pages to process per run
+# Number of pages to process per run
 BATCH_SIZE = 5
 
-#Name/path of file containing title of last processed page and namespace
+# Name/path of file containing title of last processed page and namespace
 FILE_NAME = "cat-redirects-last.txt"
 
+
 def getURL():
-	"""
-	Gets the API URL of the wiki.
+    """
+    Gets the API URL of the wiki.
 
-	Returns:
-		URL (str): API url of the wiki
-	"""
+    Returns:
+        URL (str): API url of the wiki
+    """
 
-	site = pywikibot.Site()
-	URL = site.protocol() + "://" + site.hostname() + site.scriptpath() + "/api.php"
-	return URL
+    site = pywikibot.Site()
+    URL = site.protocol() + "://" + site.hostname() + site.scriptpath() + "/api.php"
+    return URL
+
 
 def getRedirects(URL):
-	"""
-	Gets a list of all the pages containing redirects. Only returns redirects not already in Category:Redirects.
+    """
+    Gets a list of all the pages containing redirects. Only returns redirects not already in Category:Redirects.
 
-	Parameters:
-		URL (str): the API URL of the wiki
-	
-	Returns:
-	i	output (list): a list with titles of pages containing redirects
-	"""
+    Parameters:
+        URL (str): the API URL of the wiki
 
-	if not os.path.isfile(FILE_NAME):
-		last = ''
-		ns = -2
-	else:
-		with open(FILE_NAME) as reader:
-			last = reader.readline()
-			ns = int(reader.readline())
+    Returns:
+    i	output (list): a list with titles of pages containing redirects
+    """
 
-	output = set()
-	session = requests.Session()
+    if not os.path.isfile(FILE_NAME):
+        last = ''
+        ns = -2
+    else:
+        with open(FILE_NAME) as reader:
+            last = reader.readline()
+            ns = int(reader.readline())
 
-	PARAMS = {
-	"action": "query",
-	"format": "json",
-	"list": "allredirects",
-	"arunique": "1",
-	"arnamespace": "0",
-	"arlimit": BATCH_SIZE
-	}
+    output = set()
+    session = requests.Session()
 
-	request = session.get(url=URL, params=PARAMS, verify=False)
-	json = request.json()
-	
-	if not "query" in json or not "allredirects" in json["query"]:
-		return ""
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "list": "allredirects",
+        "arunique": "1",
+        "arnamespace": "0",
+        "arlimit": BATCH_SIZE
+    }
 
-	for page in json["query"]["allredirects"]:
-		output.add(page["title"])
+    request = session.get(url=URL, params=PARAMS, verify=False)
+    json = request.json()
 
-	with open(FILE_NAME, "w+") as writer:
-		try:	
-			writer.write(json["continue"]["garcontinue"] + "\n" + str(ns))
-		except:
-			if ns < 15:
-				ns += 1
-			else:
-				ns = -2
-			writer.write('' + "\n" + str(ns))
+    if not "query" in json or not "allredirects" in json["query"]:
+        return ""
 
-	CATS_PARAMS = {
-	"action": "query",
-	"list": "categorymembers",
-	"cmtitle": "Category:Redirects",
-	"cmnamespace": "*",
-	"format": "json"
-	}
+    for page in json["query"]["allredirects"]:
+        output.add(page["title"])
 
-	request = session.get(url=URL, params=CATS_PARAMS, verify=False)
-	json = request.json()
+    with open(FILE_NAME, "w+") as writer:
+        try:
+            writer.write(json["continue"]["garcontinue"] + "\n" + str(ns))
+        except:
+            if ns < 15:
+                ns += 1
+            else:
+                ns = -2
+            writer.write('' + "\n" + str(ns))
 
-	remove = set()
-	for page in json["query"]["categorymembers"]:
-		remove.add(page["title"])
+    CATS_PARAMS = {
+        "action": "query",
+        "list": "categorymembers",
+        "cmtitle": "Category:Redirects",
+        "cmnamespace": "*",
+        "format": "json"
+    }
 
-	try:
-		continueValue = json["continue"]["cmcontinue"]
-	except:
-		continueValue = ''
+    request = session.get(url=URL, params=CATS_PARAMS, verify=False)
+    json = request.json()
 
-	while continueValue:
-		CATS_PARAMS = {
-		"action": "query",
-		"list": "categorymembers",
-		"cmtitle": "Category:Redirects",
-		"cmnamespace": "*",
-		"format": "json",
-		"cmcontinue": continueValue
-		}
+    remove = set()
+    for page in json["query"]["categorymembers"]:
+        remove.add(page["title"])
 
-		request = session.get(url=URL, params=CATS_PARAMS, verify=False)
-		json = request.json()
+    try:
+        continueValue = json["continue"]["cmcontinue"]
+    except:
+        continueValue = ''
 
-		for page in json["query"]["categorymembers"]:
-			remove.add(page["title"])
+    while continueValue:
+        CATS_PARAMS = {
+            "action": "query",
+            "list": "categorymembers",
+            "cmtitle": "Category:Redirects",
+            "cmnamespace": "*",
+            "format": "json",
+            "cmcontinue": continueValue
+        }
 
-		try:
-			continueValue = json["continue"]["cmcontinue"]
-		except:
-			continueValue = ''
-	
-	return output.difference(remove)
+        request = session.get(url=URL, params=CATS_PARAMS, verify=False)
+        json = request.json()
+
+        for page in json["query"]["categorymembers"]:
+            remove.add(page["title"])
+
+        try:
+            continueValue = json["continue"]["cmcontinue"]
+        except:
+            continueValue = ''
+
+    return output.difference(remove)
+
 
 def addToCategory(title, category):
-	"""
-	Adds a page to a category.
+    """
+    Adds a page to a category.
 
-	Parameters:
-		title (str): the title of the page to be added to a category
-		category (str): the category to add the page to
-	"""
+    Parameters:
+        title (str): the title of the page to be added to a category
+        category (str): the category to add the page to
+    """
 
-	if title == " ":
-		print("Invaild Title")
-		return 
+    if title == " ":
+        print("Invaild Title")
+        return
 
-	page = pywikibot.Page(pywikibot.Site(), title)
-	text = page.text 
-	
-	 
+    page = pywikibot.Page(pywikibot.Site(), title)
+    text = page.text
 
-	if match_category(category, text):
-		print("Page is already in %s! Skipping %s..." % (category, title))
-	else:
-		print("Page is not in %s! Adding %s..." % (category, title))
-		page.text += category
-		page.save("Add to ")	
+    if match_category(category, text):
+        print("Page is already in %s! Skipping %s..." % (category, title))
+    else:
+        print("Page is not in %s! Adding %s..." % (category, title))
+        page.text += category
+        page.save("Add to ")
+
 
 def match_category(category, text):
-	
-	test = "\[{2}\s*Category\s*:\s*" + category + "\s*\]{2}"
+    """
+    Gets the name of category and the title of the page
+    then finds out if the page needs to be in redirects or
+    if the page is already in the redirects page.
 
-	return re.match(test, text, re.DOTALL | re.IGNORECASE)
+    Returns:
+        True or False
+    """
+    test = "\[{2}\s*Category\s*:\s*" + category + "\s*\]{2}"
+
+    return re.match(test, text, re.DOTALL | re.IGNORECASE)
 
 
 def main():
-
-	for title in getRedirects(getURL()):
-		addToCategory(title, "Redirects")
+    """
+    Iterates through the wiki and adds pages to redirects page
+    if needed.
+    """
+    for title in getRedirects(getURL()):
+        addToCategory(title, "Redirects")
 
 
 if __name__ == '__main__':
-	main()
+    main()
